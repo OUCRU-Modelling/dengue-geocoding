eda476b936410600df4d1f0ebcbb3091ec054109
osupplisson <osupplis@gmail.com>
2026-01-28 16:57:02 +0100

    Add OSM + kmprojection Add aggregation of HCMC shapefiles + kmproj

--- codes/00_packages.R
+++ codes/00_packages.R
@@ -1,12 +1,26 @@
 # Packages ----------------------------------------------------------------
-install.packages("remotes")
-remotes::install_gitlab ("ropensci/osmdata")
-remotes::install_github("r-spatial/sf")
-remotes::install_github("rspatial/terra")
-remotes::install_github("inlabru-org/fmesher")
+#install.packages("remotes")
+#install.packages("pak")
 
+
+#remotes::install_gitlab ("ropensci/osmdata")
+#remotes::install_github("r-spatial/sf")
+#remotes::install_github("rspatial/terra")
+#remotes::install_github("inlabru-org/fmesher")
+#remotes::install_github("sfirke/janitor")
+#pak::pak("tidyverse/tidyverse")
 library("osmdata")
 library("sf")
 library("terra")
 library("fmesher")
+library("tidyverse")
 
+
+
+# VN projection and HCMC spatial extent -----------------------------------------
+# Roughly HCMC spatial extent
+cropvector <- c(106.2, 107.2, 10.2, 11.3)
+hcmc_extent <- ext(cropvector)
+
+#VN projection
+kmproj <-  fm_crs_set_lengthunit(st_crs("EPSG:9210"),  "km") 
---
+++ codes/01_import_hcmc_shapefile_pre_refrom.R
@@ -0,0 +1,58 @@
+# Packages ----------------------------------------------------------------
+source("codes/00_packages.R", verbose = T)
+
+
+# Shapefile ---------------------------------------------------------------
+# From https://gadm.org/download_country.html
+urban_unit <-
+  read_sf("data/gadm/gadm41_VNM_shp/gadm41_VNM_3.shp",
+          options = "ENCODING=UTF-8"
+  ) %>%
+  janitor::clean_names() %>%
+  st_crop(
+    xmin = cropvector[1],
+    xmax = cropvector[2],
+    ymin = cropvector[3],
+    ymax = cropvector[4]
+  ) %>%
+  mutate(
+    name_2 = ifelse(str_detect(name_1, "Minh"), name_2, "Outside HCMC"),
+    name_3 = ifelse(str_detect(name_1, "Minh"), name_3, "Outside HCMC")
+  ) %>%
+  mutate(name_2 = stringi::stri_trans_general(name_2, "Latin-ASCII")) %>%
+  mutate(name_3 = stringi::stri_trans_general(name_3, "Latin-ASCII")) %>%
+  st_transform(kmproj)
+
+# Initial CRS
+crssave <- crs(urban_unit)
+
+### Aggregating geometry at the country level
+boundary_hcmc <- urban_unit %>%
+  filter(name_2 != "Outside HCMC") %>%
+  summarise() %>%
+  st_transform(kmproj)
+
+
+### Aggregating the geometry at the district level
+hcmc_district <- urban_unit %>%
+  group_by(name_2) %>%
+  summarise() %>%
+  st_transform(kmproj)
+
+### Aggregating commune level
+hcmc_commune <- urban_unit %>%
+  group_by(name_3) %>%
+  summarise() %>%
+  st_transform(kmproj)
+
+
+### Old-school HCMC shapefiles
+shapefiles_hcmc_pre_reform <- list(
+  "urban_unit" = urban_unit,
+  "boundary_hcmc" = boundary_hcmc,
+  "hcmc_district" = hcmc_district,
+  "hcmc_commune" = hcmc_commune
+)
+
+###Save
+saveRDS(shapefiles_hcmc_pre_reform, "data/gadm/shapefiles_hcmc_pre_reform.RDS")

afd5c5bddd0b23b6e7c1ffc208a6cbb8e599b64a
Quynh Anh <90673616+quingzz@users.noreply.github.com>
2026-02-02 17:39:34 +0700

    cache using qs, improve address cleaning

--- geocoding/geocoding.R
+++ geocoding/geocoding.R
@@ -5,13 +5,14 @@ library(tidygeocoder)
 library(readxl)
 library(tidyverse)
 library(vietnameseConverter)
+library(stringi)
+library(qs2)
 
 # ==== Util funcs =====
-
 # - Remove id in address
 # - Resolve encoding error (for data fr 2000-2008)
 # - Generate raw_addr column for geocoding
-preprocess_addr <- function(data, decode_addr=FALSE){
+preprocess_addr <- function(data, decode_addr=FALSE, remove_accent=TRUE){
   data %>%
     mutate(
       # sometimes addr have ID in it, remove if it's the case
@@ -23,6 +24,8 @@ preprocess_addr <- function(data, decode_addr=FALSE){
       ),
       # if specified, encode address from TCVN/VISCII to UTF-8 (i.e. resolve mojibake)
       Diachi = if (decode_addr) vietnameseConverter::decodeVN(Diachi, from = "TCVN3") else Diachi,
+      Diachi = if (remove_accent) stri_trans_general(Diachi, id = "Latin-ASCII") else Diachi,
+      Diachi = str_to_title(Diachi),
       # generate raw address to geocode
       raw_addr = paste0(
         case_when(
@@ -71,25 +74,26 @@ geocode_by_batch <- function(x, batch_size=5, sleep=1){
 # NOTE: Too Many Requests (RFC 6585) (HTTP 429) when LIMIT over 100
 # TODO: Contact Vietmap for the request limit per minute/second
 # Maybe geocode by batch of 100
-LIMIT <- 100
-SHEET <- "2016"
+LIMIT <- 200 # Limit number of addresses being geocoding when running this script
+
 
 # TODO: load merged dataset (?) instead
 # - Also make a wrapper func for safe file read
 # --------------
+SHEET <- "2016"
 CURR_YEAR <- as.numeric(SHEET)
 cases_dat <- read_excel("./data/incidence/2000_2016_updated.xlsx", sheet=SHEET)
 # --------------
 
-cached_dat <- readRDS("./data/cached/geocoded_addr.rds")
-
+# cached_dat <- readRDS("./data/cached/geocoded_addr.rds")
+cached_dat <- qs_read("./data/cached/geocoded_addr.qs")
 
 # ====== Get unique addresses ======
 preprocessed_dat <- cases_dat %>%
   mutate(
     id = if_else(is.na(Maso), `Ma moi BC`, Maso)
   ) %>%
-  preprocess_addr(decode_addr = CURR_YEAR<2009) %>%
+  preprocess_addr(decode_addr = CURR_YEAR<2009, remove_accent = FALSE) %>%
   select(raw_addr) %>%
   unique()
 
@@ -108,6 +112,7 @@ out <- preprocessed_dat %>%
     geo = geocode_by_batch(raw_addr, batch_size = 100, sleep=60)
   ) %>%
   unnest(geo)
+
 # out %>% View()
 
 # ====== Cache output ========
@@ -115,11 +120,12 @@ out <- preprocessed_dat %>%
 new_cache <- bind_rows(cached_dat,
                        out) %>%
   unique()
-# new_cache
+
+# new_cache %>% View()
 
 # save the data.frame of geocoded address
-saveRDS(new_cache, "./data/cached/geocoded_addr.rds")
-
+# saveRDS(new_cache, "./data/cached/geocoded_addr.rds")
+qs_save(new_cache, "./data/cached/geocoded_addr.qs")
 
 
 

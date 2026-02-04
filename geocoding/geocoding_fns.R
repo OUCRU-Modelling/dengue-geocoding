0ee1ebdde9e33767be2f7726cc1c079df7e99044
Quynh Anh <90673616+quingzz@users.noreply.github.com>
2026-02-04 17:50:39 +0700

    update manual geocoding func

--- geocoding/geocoding_fns.R
+++ geocoding/geocoding_fns.R
@@ -4,6 +4,8 @@
 library(tidygeocoder)
 library(tidyverse)
 library(qs2)
+library(terra)
+library(sf)
 
 # ==== Util funcs =====
 # geocode by batch of batch_size with sleep time in between batches
@@ -26,14 +28,116 @@ geocode_by_batch <- function(x, batch_size=5, sleep=1){
 
     if(end < length(x)) Sys.sleep(sleep)
 
+    if(nrow(out %>% filter(!is.na(long))) > 0){
+      out %>%
+        rename(
+          vietmap_district = district,
+          vietmap_district_id = district_id,
+          vietmap_ward = ward,
+          vietmap_ward_id = ward_id
+        ) %>%
+        select(-address,-name, -hs_num, -vietmap_street, -vietmap_city)
+    }
+
     out
   }) %>% bind_rows()
 }
 
+# helper function to generate a tibble of district/ward and their centroid
+get_centroid_df <- function(gadm_file,
+                            select_cols = c("NAME_2", "NAME_3")){
+  dat <- read_rds(gadm_file) %>%
+    terra::unwrap()
+  # keep polygons for hcm city only
+  dat <- dat[dat$GID_1 == "VNM.25_1", ]
+
+  dat %>%
+    centroids(inside=TRUE) %>%
+    st_as_sf() %>%
+    mutate(
+      long = st_coordinates(geometry)[,1],
+      lat = st_coordinates(geometry)[,2]
+    ) %>%
+    as_tibble() %>%
+    select( any_of( c(select_cols, "long", "lat") ) ) %>%
+    # preprocess district, ward name
+    mutate(across(where(is.character), \(str){
+      str_to_upper(str) %>%
+        stri_trans_general(id = "Latin-ASCII") %>%
+        # make sure the name match that in the incidence data
+        if_else(
+          str_detect(., "\\d"),
+          str_remove(., "(QUAN |PHUONG )"),
+          .
+        ) %>%
+        str_pad(width = 2, side = "left", pad = "0")
+    }))
+}
+
+
+# ========= Geocode functions ==========
+# Manually geocode addresses given ward/district
+# Return the centroid of the ward/district
+geocode_manual <- function(df,
+                           addr_col = "raw_addr",
+                           admin3_col = "px",
+                           admin2_col = "qh",
+                           na_label = c("KHONG RO", "THI TRAN"),
+                           admin3_gadm = "./data/gadm/gadm41_VNM_3_pk.rds",
+                           admin2_gadm = "./data/gadm/gadm41_VNM_2_pk.rds"){
+
+  # ------ Get the centroids -------
+  admin3 <- get_centroid_df(admin3_gadm)
+  admin2 <- get_centroid_df(admin2_gadm)
+
+  # ------ Get addresses to geocode ------
+  to_geocode <- df %>%
+    select(all_of(c(addr_col, admin2_col, admin3_col))) %>%
+    unique() %>%
+    rename(
+      qh = admin2_col,
+      px = admin3_col
+    ) %>%
+    # require at least district or ward to geocode
+    filter(!(px %in% na_label) | !(qh %in% na_label))
+
+  # ------ Geocode manually (i.e., the centroid of district/ward) ------
+  out <- to_geocode %>%
+    # geocode using ward if available, fallback to district otherwise
+    mutate(
+      geocode_lvl = if_else(
+        !(px %in% na_label),
+        "admin3",
+        "admin2"
+      )
+    ) %>%
+    group_by(geocode_lvl) %>%
+    group_modify(\(.x, .y){
+      if(.y$geocode_lvl == "admin3"){
+        return(
+          .x %>%
+            left_join(admin3, by =
+                        join_by(px == NAME_3, qh == NAME_2))
+        )
+      }
+
+      .x %>%
+        left_join(admin2, by = join_by(qh == NAME_2))
+    }) %>%
+    ungroup() %>%
+    select(-geocode_lvl)
+
+  list(
+    out = out %>% filter(!is.na(long), !is.na(lat)),
+    failed = out %>% filter(is.na(long) | is.na(lat))
+  )
+}
+
+# Geocode addresses and append new results to a cached file
 # cache_path: path to .qs file that store geocoded addresses
 # colname: name of the column for address in df
-# limit: max no. of addresses to be geocoded per geocode_df() call
-# batch: no. addresses per tidygeocoder::geo() call. Vietmap may need "cool down" time per 100ish requests.
+# limit: max no. of addresses to be geocoded via API call per geocode_df() call
+# batch: no. addresses per tidygeocoder::geo() call. Vietmap API may need "cool down" time per 100ish requests (free tier).
 # sleep: sleep time between batches (in seconds)
 geocode_df <- function(df, cache_path,
                        colname = "raw_addr",
@@ -66,17 +170,24 @@ geocode_df <- function(df, cache_path,
       geo = geocode_by_batch(raw_addr, batch_size = batch, sleep = sleep)
     ) %>%
     unnest(geo)
+  # only cache addresses that were successfully geocoded
+  success <- out %>% filter(!is.na(long) && !is.na(lat))
+  fail <- out %>% filter(is.na(long) | is.na(lat))
 
   # ------ Cache output -------
   new_cache <- bind_rows(cached_dat,
-                         out) %>%
+                         success) %>%
     unique()
   qs_save(new_cache, cache_path)
 
-  new_cache
+  list(
+    out = new_cache,
+    failed = fail,
+    msg = paste0(
+      nrow(success),
+      " out of ",
+      nrow(out),
+      " addresses successfully geocoded and appended to cache"
+    )
+  )
 }
-
-
-
-
-
--- geocoding/main.R
+++ geocoding/main.R
@@ -1,4 +1,4 @@
-# TODO: turn this into a script
+# TODO: make this a script with args
 
 source("geocoding/geocoding_fns.R")
 source("geocoding/clean_data_fns.R")
@@ -6,23 +6,40 @@ source("geocoding/clean_data_fns.R")
 # ======= Data processing =======
 df_2000_2016 <- ingest_xlsx("./data/incidence/2000_2016_updated.xlsx")
 
-
 test_clean_df <- df_2000_2016 %>% clean_xlsx(remove_accent = FALSE)
 test_no_accent <- df_2000_2016 %>% clean_xlsx(remove_accent = TRUE)
 
-test_clean_addr <- test_clean_df %>% get_clean_addr(colname = "raw_addr")
-test_clean_no_accent <- test_no_accent %>% get_clean_addr(colname = "raw_addr")
-
+# Test number of addresses w/ and w/out removing accents
+# test_clean_addr <- test_clean_df %>% get_clean_addr(colname = "raw_addr")
+# test_clean_no_accent <- test_no_accent %>% get_clean_addr(colname = "raw_addr")
 # Note: with accent 147927 -> 135387 after get_clean_addr()
 # Note: without accent 147927 -> 134081 after get_clean_addr()
 
 # ====== Geocoding data =========
-LIMIT <- 1 # max no. of addresses to be geocoded per geocode_df() call
-BATCH <- 1 # no. addresses per tidygeocoder::geo() call. Vietmap need "cool down" time per 100ish requests (free tier).
-SLEEP <- 60 # sleep time between batches (in seconds)
+# Manual geocoding ------
+# If specific address is not available --> use centroid of ward/district as geocode
+manual_out <- test_clean_df %>%
+  filter(is.na(diachi)) %>%
+  geocode_manual(
+    addr_col = "raw_addr",
+    admin3_col = "px",
+    admin2_col = "qh",
+    na_label = c("KHONG RO", "THI TRAN"),
+    admin3_gadm = "./data/gadm/gadm41_VNM_3_pk.rds",
+    admin2_gadm = "./data/gadm/gadm41_VNM_2_pk.rds"
+  )
 
-out <- test_clean_df %>%
-  head(n=50) %>%
+message("Manual geocoding: ", nrow(manual_out$out), " addresses successfully geocoded, while ",
+        nrow(manual_out$failed), " failed")
+
+# Geocoding using API call ------
+# Settings for geocoding using Vietmap API
+LIMIT <- 1 # max no. of addresses to be geocoded via Vietmap API call per geocode_df() call
+BATCH <- 1 # no. addresses per tidygeocoder::geo() call. Vietmap need "cool down" time per 100ish requests (free tier).
+SLEEP <- 62 # sleep time between batches (in seconds)
+
+api_out <- test_clean_df %>%
+  filter(!is.na(diachi)) %>%
   geocode_df(
     cache_path = "./data/cached/geocoded_addr.qs",
     colname = "raw_addr",
@@ -30,4 +47,5 @@ out <- test_clean_df %>%
     batch = BATCH,
     sleep = SLEEP
   )
-nrow(out)
+
+message("Vietmap geocoding: ", api_out$msg)

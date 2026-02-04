b6eb6138295d2e98c331ab0fae40f553d0fff2c4
Tuyen Huynh <hntuyen282@gmail.com>
2026-02-04 11:51:11 +0700

    sync renv

--- codes/00_packages.R
+++ codes/00_packages.R
@@ -1,14 +1,4 @@
 # Packages ----------------------------------------------------------------
-#install.packages("remotes")
-#install.packages("pak")
-
-
-#remotes::install_gitlab ("ropensci/osmdata")
-#remotes::install_github("r-spatial/sf")
-#remotes::install_github("rspatial/terra")
-#remotes::install_github("inlabru-org/fmesher")
-#remotes::install_github("sfirke/janitor")
-#pak::pak("tidyverse/tidyverse")
 library("osmdata")
 library("sf")
 library("terra")

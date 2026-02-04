f326cc3c58f05a06fb1c5fbab44346d88d8eb18a
Tuyen Huynh <hntuyen282@gmail.com>
2026-02-04 11:51:17 +0700

    formatting

--- codes/00_packages.R
+++ codes/00_packages.R
@@ -5,12 +5,10 @@ library("terra")
 library("fmesher")
 library("tidyverse")
 
-
-
-# VN projection and HCMC spatial extent -----------------------------------------
+# VN projection and HCMC spatial extent -----------------------------------
 # Roughly HCMC spatial extent
 cropvector <- c(106.2, 107.2, 10.2, 11.3)
 hcmc_extent <- ext(cropvector)
 
 #VN projection
-kmproj <-  fm_crs_set_lengthunit(st_crs("EPSG:9210"),  "km") 
+kmproj <- fm_crs_set_lengthunit(st_crs("EPSG:9210"), "km")

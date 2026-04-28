07dcb9ba80b6bef587311619562f988dcdf5d28e
Quynh Anh <90673616+quingzz@users.noreply.github.com>
2026-04-28 13:27:33 +0700

    enforcing exlusion criteria for geocoding BD VT

--- codes/02_generate_to_geocode_bd_vt.R
+++ codes/02_generate_to_geocode_bd_vt.R
@@ -9,10 +9,22 @@ to_geocode_bd_vt_2017_2025 <- incidence_bd_vt_2017_2025 %>%
   unique()
 
 to_geocode_bd_vt <- to_geocode_bd_vt_2017_2025 %>%
+  mutate(
+    # enforce stricter condition, consider addresses as missing when:
+    # - diachi contains all alphabet characters/space
+    # - diachi contains all numeric/space
+    diachi = case_when(
+      str_detect(diachi, "^[a-zA-Z ]+$") ~ NA,
+      str_detect(diachi, "^[^a-zA-Z ]+$") ~ NA,
+      .default = diachi
+    )
+  ) %>%
   filter(
     !is.na(diachi),
     # only geocode diachi with at least 3 characters
-    str_length(diachi)>3
+    str_length(diachi)>3,
+    # only geocode diachi with at least 2 consecutive alphabet characters
+    str_detect(diachi, "[A-Za-z][A-Za-z]+")
   )
 
 dim(to_geocode_bd_vt)
